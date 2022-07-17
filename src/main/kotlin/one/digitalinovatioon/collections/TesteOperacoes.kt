2b906a0b8021e023ce4c96dc4c018b4f4154f0a8
suellenmuniz <su09muniz@hotmail.com>
2022-07-17 09:42:35 -0300

    created collection operations

---
+++ src/main/kotlin/one/digitalinovatioon/collections/TesteOperacoes.kt
@@ -0,0 +1,31 @@
+package one.digitalinovatioon.collections
+
+fun main() {
+    val salarios = doubleArrayOf(2000.0, 2200.0, 3300.0)
+
+    for (salario in salarios) {
+        println(salario)
+    }
+
+    println("----------------------")
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
+    println("Media salarial: ${salarios.average()}")
+
+    val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
+    println("----------------------")
+    salariosMaiorQue2500.forEach { println(it) }
+
+    println("---------------------------")
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("----------------------")
+    println(salarios.find { it == 2200.0 })
+    println(salarios.find { it == 500.0 })
+
+    println("----------------------")
+    println(salarios.any { it == 3300.0 })
+    println(salarios.any { it == 500.0 })
+
+
+}
