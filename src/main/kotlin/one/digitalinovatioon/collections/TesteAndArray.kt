c4a3b647cafd2e816d6629256b30acab7875bef9
suellenmuniz <su09muniz@hotmail.com>
2022-07-17 08:37:55 -0300

    created int array test class

---
+++ src/main/kotlin/one/digitalinovatioon/collections/TesteAndArray.kt
@@ -0,0 +1,32 @@
+package one.digitalinovatioon.collections
+
+fun main() {
+    val values = IntArray(5)
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for (valor in values) {
+        println(valor)
+    }
+
+    println("-----------------------------")
+    values.forEach { valor ->
+        println(valor)
+    }
+
+    println("-----------------------------")
+    for (Index in values.indices) {
+        println(values[Index])
+    }
+
+    println("-----------------------------")
+
+    values.sort()
+    for (valor in values) {
+        println(valor)
+    }
+}
+
